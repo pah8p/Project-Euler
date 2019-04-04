0a488e80211aee02b996a5fe4e60c9c8fc1b3afa
pete <phoward24@gmail.com>
2019-04-03 21:37:00 -0700

    circles on 88

--- problem_88.py
+++ problem_88.py
@@ -1,36 +1,10 @@
 import euler_tools
 import itertools
 import functools
+import pickle
 
-##@functools.lru_cache(maxsize=256)
-##def ways(target, value):
-##
-##    if value == 1:
-##        r = [[1]*target]
-##
-##    else:
-##
-##        s = []
-##        top = int(target/value) + 1
-##        for n in range(0, top):
-##            remainder = target - value*n
-##            subways = ways(remainder, value -1)
-##            for subway in subways:
-##                s.append(n*[value] + subway)
-##                 
-##        r = s
-##
-##    return r
-
-WAYS = {}
-
-def ways(target, value):
-
-    try:
-        return WAYS[(target, value)]
-
-    except KeyError:
-
+@euler_tools.Memoize
+def partitions(target, value):
         if value == 1:
             r = [[1]*target]
 
@@ -46,92 +20,138 @@ def ways(target, value):
                      
             r = s
 
-        #if target < 20:
-        WAYS[(target, value)] = r
-
         return r
 
-SPS = {}
-
+#@euler_tools.Memoize
+#@functools.lru_cache(maxsize=1024)
 def sums_products(target, value):
+    
+    '''(sum, product, size)'''
+    if value == 1:
+        return [(target, 1, target)]
 
-    try:
-        return SPS[(target, value)]
+    else:
+        
+        s = []
+        top = int(target/value) + 1
+        for n in range(0, top):
+            remainder = target - value*n
+            subways = sums_products(remainder, value - 1)
+            #DEPS[(target, value)].append((remainder, value-1))
+            for subway in subways:
 
-    except KeyError:
-
-        '''(sum, product, size)'''
-
-        if value == 1:
-            r = [(target, 1, target)]
-
-        else:
-
-            s = []
-            top = int(target/value) + 1
-            for n in range(0, top):
-                remainder = target - value*n
-                subways = sums_products(remainder, value - 1)
-                for subway in subways:
+                if subway[1] * value**n < 15e3:
                     s.append((
                         subway[0] + n*value,
                         subway[1] * value**n,
                         subway[2] + n,
                     ))
 
-            r = s
+        return s
 
-        #if target < 20:
-        SPS[(target, value)] = r
+def sums_products_hd(target, value):
+    file_name = 'p88_cache/%s_%s.pickle' % (target, value)
+    try:
+        with open(file_name, 'rb') as f:
+            s = pickle.load(f)
 
-        return r
+    except FileNotFoundError:
 
-def product(iterable):
-    p = 1
-    for i in iterable:
-        p *= i
-    return p
+        if value == 1:
+            return [(target, 1, target)]
 
-def min_psm(n, k):
+        else:
+            
+            s = []
+            top = int(target/value) + 1
+            for n in range(0, top):
+                remainder = target - value*n
+                subways = sums_products_hd(remainder, value - 1)
+                for subway in subways:
+
+                    if subway[1] * value**n < 10*target:
+                        s.append((
+                            subway[0] + n*value,
+                            subway[1] * value**n,
+                            subway[2] + n,
+                        ))
+
+
+
+        with open(file_name, 'wb') as f:
+            pickle.dump(s, f)
+
+    return s
+
+def products_sums(n):
+    primes = euler_tools.prime_divisors_2(n)
+    print(primes)
+
+def min_psm(k_max):
+
+    ks = list(range(2, k_max+1))
+    psms = []
     psm = {}
-    for i in range(2, n):
-        with euler_tools.Watch(i):
-            ways_to_sum = ways(i, i)
-        for way_to_sum in ways_to_sum:
+    n = 4
+    while True:
+        with euler_tools.Watch(n):
+            sps = sums_products_hd(n, n)
+        for sp in sps:
+            if sp[2] > 1:
+                if sp[0] == sp[1]:
 
-            if sum(way_to_sum) == product(way_to_sum):
+                    psms.append(sp)
+                    try:
+                        if sp[0] < psm[sp[2]]:
+                            psm[sp[2]] = sp[0]
 
-                try:
-                    curr_min = psm[len(way_to_sum)]['min']
-                    if i < curr_min:
-                        psm[len(way_to_sum)] = {
-                            'min': i,
-                            'way': way_to_sum,
-                        }
-                except KeyError:
-                    psm[len(way_to_sum)] = {
-                        'min': i,
-                        'way': way_to_sum,
-                    }
+                    except KeyError:
+                        if sp[2] in ks:                        
+                            ks.pop(ks.index(sp[2]))
+                            psm[sp[2]] = sp[0]
+                     
+                   
+        n += 1
+        if n % 10 == 0: print(n, ks)
+       
+        if not ks:
+            break
 
-    mins = list()
-    for key, val in psm.items():
-        print(key, val)
-        if 2 <= key <= k:
-            mins.append(val['min'])
+    import pandas
+    df = pandas.DataFrame(psms)
+    df.to_csv('product_sum.csv', index=False)
 
-    return sum(set(mins)), max(psm.keys())
+
+    return sum(set(psm.values()))
+
 
 
 with euler_tools.Watch():
-    #print(ways(5, 5))
-    print(ways(5, 5))
-    print(sums_products(12, 12))
-##   psms = min_psm(50, 20)
-##    print(psms)
-##    for k, v in psms.items():
-##        print(k, v['min'])
+    #print(sums_products(4, 4))
     
-#for k in sorted(WAYS.keys()):
-#    print(k)
+    #for i in range(5):
+    #    for j in range(1, 5):
+    #        print(i, j, sums_products(i, j))
+    
+    #for k, v in DEPS.items():
+    #        print(k, v)    
+
+    #print(sums_products(6, 6))
+    #print(sums_products(8, 8))
+    #print(sums_products(12, 12))
+    #print(sums_products(15, 15))
+    #print(ways(50, 50))
+    #print(sums_products(5, 5))
+    #print(min_psm(50))
+    #print(sums_products.cache_info())
+    #print(sorted(COUNTS.items(), key=lambda x: x[1]))
+    #for x in sums_products(15, 15): 
+    #    if x[0] == x[1]: print(x)
+    #print(sums_products_2(4))
+    #print(sums_products_2(6))
+    #print(sums_products_2(8))
+    #print(sums_products_2(12))
+    #print(sums_products_2(15))
+    #print(sums_products_2(16))
+    #print(list(set(itertools.chain(*[sums_products_2(n) for n in range(4, 12000)]))))
     
